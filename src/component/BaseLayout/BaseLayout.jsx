96015d1e571a22239ecbeeb9fb948e1de172823b
jayanthbr29 <jayanthbr@digi9.co.in>
2025-06-26 12:40:37 +0530

    scroll fix

--- src/component/BaseLayout/BaseLayout.jsx
+++ src/component/BaseLayout/BaseLayout.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import { PageWrapper, ContentWrapper } from "../BaseLayout/BaseLayout.styles";
-import Sidebar from "../Sidebar/Sidebar";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
+  import React from "react";
+  import { Outlet } from "react-router-dom";
+  import { PageWrapper, ContentWrapper } from "../BaseLayout/BaseLayout.styles";
+  import Sidebar from "../Sidebar/Sidebar";
+  import Header from "../Header/Header";
+  import Footer from "../Footer/Footer";
 
-const BaseLayout = () => {
-  return (
-    <PageWrapper>
+  const BaseLayout = () => {
+    return (
+      <PageWrapper>
 
-      <Sidebar />
-      <ContentWrapper>
-      <Header />
-        <Outlet />
-        <Footer />
-      </ContentWrapper>
-    </PageWrapper>
-  );
-};
+        <Sidebar />
+        <ContentWrapper>
+        <Header />
+          <Outlet />
+          <Footer />
+        </ContentWrapper>
+      </PageWrapper>
+    );
+  };
 
-export default BaseLayout;
+  export default BaseLayout;
